89147468e7af8915f40f18881192d478876eb65f
Ahmed Tarek <ahmedtarik969@gmail.com>
2022-07-11 12:08:21 +0200

    First

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExaminationSystemProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
