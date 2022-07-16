3e5671729691875e563f2f000a0295684c115372
amrafifi11111@outlook.sa <amrafifi11111@outlook.sa>
2022-07-16 13:47:00 +0200

    asdddd

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using ExaminationSystemProject.ViewModel;
+﻿using ExaminationSystemProject.Models;
+using ExaminationSystemProject.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,15 +9,18 @@ namespace ExaminationSystemProject.Controllers
     [Authorize(Roles = ("Admin"))]
     public class RolesController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly RoleManager<IdentityRole> rolemanager;
 
-        public RolesController(RoleManager<IdentityRole> _roleManager)
+        public RolesController(RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> _userManager)
         {
             this.rolemanager = _roleManager;
+            this.userManager = _userManager;
         }
         [HttpGet]
         public IActionResult New()
         {
+            
             return View();
         }
         [HttpPost]
@@ -36,8 +40,7 @@ namespace ExaminationSystemProject.Controllers
                     ModelState.AddModelError("", item.Description);
                 }
             }
-            
-            return View(RoleName);
+                        return View(RoleName);
         }
     }
 }
